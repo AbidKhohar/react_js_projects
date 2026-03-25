08455e98c09fa1e4d08b920846aebfeb99de4ace
abidkhokhar628-hue <abidasif125@gmail.com>
2026-03-25 16:33:37 +0500

    react js workings

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ClassComponents from './Components/ClassComponents'
+import FunctionComponents from './Components/FunctionComponents'
+
+function App() {
+  return (
+    <div>
+      <h1>Vite App</h1>
+      <FunctionComponents/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/FunctionComponents.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react'
+
+function FunctionComponents() {
+
+  const [name, setName] = useState("")
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <h1>React Function Component</h1>
+
+      <input
+        type="text"
+        placeholder="Enter your name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+
+      <h2>Your Name: {name}</h2>
+
+      <h2>Count: {count}</h2>
+
+      <button onClick={() => setCount(count + 1)}>
+        Increase
+      </button>
+
+      <button onClick={() => setCount(count - 1)}>
+        Decrease
+      </button>
+    </div>
+  )
+}
+
+export default FunctionComponents
